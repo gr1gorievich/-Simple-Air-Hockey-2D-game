308f76d6bf28245fc3ad36d9890f847178ff4c33
Roman Madan <tabsentoday@gmail.com>
2019-06-30 20:29:21 +0300

    Create Paddle.cpp

---
+++ Paddle.cpp
@@ -0,0 +1,32 @@
+#include "Paddle.h"
+
+Paddle::Paddle(SDL_Renderer* renderer) : Entity(renderer) {
+	// Load texture
+	SDL_Surface* surface = IMG_Load("paddle.png");
+	texture = SDL_CreateTextureFromSurface(renderer, surface);
+	SDL_FreeSurface(surface);
+
+	width = 32;
+	height = 128;
+
+	x = 0;
+	y = 0;
+}
+
+Paddle::~Paddle() {
+	// Clean resources
+	SDL_DestroyTexture(texture);
+}
+
+void Paddle::Update(float delta) {
+
+}
+
+void Paddle::Render(float delta) {
+	SDL_Rect rect;
+	rect.x = (int)(x + 0.5f); // Round the float to the nearest integer
+	rect.y = (int)(y + 0.5f); // Round the float to the nearest integer
+	rect.w = width;
+	rect.h = height;
+	SDL_RenderCopy(renderer, texture, 0, &rect);
+}
