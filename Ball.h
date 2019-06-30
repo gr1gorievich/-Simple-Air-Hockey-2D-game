8fa210666104bbea4a52ceef0700076b66ef1530
Roman Madan <tabsentoday@gmail.com>
2019-06-30 20:24:11 +0300

    Create Ball.h

---
+++ Ball.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#ifndef BALL_H_
+#define BALL_H_
+
+#include "Entity.h"
+#include <math.h>
+
+// Define a ball speed in pixels per second
+const float BALL_SPEED = 1100;
+
+class Ball : public Entity {
+public:
+	Ball(SDL_Renderer* renderer);
+	~Ball();
+
+	void Update(float delta);
+	void Render(float delta);
+
+	void SetDirection(float dirx, float diry);
+
+	float dirx, diry;
+
+private:
+	SDL_Texture* texture;
+
+};
+
+#endif
